11f76c118bdcdfe3b27f4db82e4b2b9a4ec8cfa5
Ram W2W <ramw2w@Rams-MacBook-Pro.local>
2024-04-11 10:58:56 +0100

    user form error validation message customized

--- src/main/java/com/spring/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/spring/blog/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.spring.blog.exceptions;
 import com.spring.blog.payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +20,16 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse(message, false);
 
         return  new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException exception){
+        Map<String, String> errorResp = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError) error).getField();
+            String mess = error.getDefaultMessage();
+            errorResp.put(fieldName, mess);
+        });
+        return new ResponseEntity<Map<String, String>>(errorResp, HttpStatus.BAD_REQUEST);
     }
 }
