947ac95bd1c1fdd6e75d542e7560aaaf35b93e78
Ram W2W <ramw2w@Rams-MacBook-Pro.local>
2024-04-11 17:49:46 +0100

    category public in interface removed

--- src/main/java/com/spring/blog/services/CategoryService.java
+++ src/main/java/com/spring/blog/services/CategoryService.java
@@ -6,9 +6,9 @@ import com.spring.blog.payloads.UserDto;
 import java.util.List;
 
 public interface CategoryService {
-    public CategoryDto createNewCategory(CategoryDto categoryDto);
-    public CategoryDto updateCategory(CategoryDto categoryDto, Integer id);
-    public CategoryDto showCategory(Integer id);
-    public void deleteCategory(Integer id);
-    public List<CategoryDto> fetchAllCategories();
+    CategoryDto createNewCategory(CategoryDto categoryDto);
+    CategoryDto updateCategory(CategoryDto categoryDto, Integer id);
+    CategoryDto showCategory(Integer id);
+    void deleteCategory(Integer id);
+    List<CategoryDto> fetchAllCategories();
 }
